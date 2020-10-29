34a984935127f0bc6a0b40dbb4f426fc69483bbf
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-10-29 17:12:24 +0900

    userモデルのバリデーション、アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,20 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :job
+  belongs_to_active_hash :gender
+  has_many :books
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
-
   with_options presence: true do
-    # 後々正規表現で細かいバリデーションを加える
     validates :nickname
-    validates :age
-    validates :job_id
-    validates :gender_id
     validates :introduce
+    validates :age, format: {with: /\A[0-9]+\z/}
+    with_options numericality: { other_than: 1 } do
+      validates :job_id
+      validates :gender_id
+    end
   end
 
 end
