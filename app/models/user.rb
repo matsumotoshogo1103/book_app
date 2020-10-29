40810fd2b88ee819e77a4d97ba2887a36d983df7
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-10-29 12:11:49 +0900

    bookモデルのバリデーションとアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :books
+
   with_options presence: true do
     # 後々正規表現で細かいバリデーションを加える
     validates :nickname
