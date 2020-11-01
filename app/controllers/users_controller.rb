6dfb6fbf478b46810c01af737b3d5d89f2e1ee44
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-11-01 17:12:11 +0900

    ルーティングとパスの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,8 @@ class UsersController < ApplicationController
   def index
     @user = User.order('id DESC')
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'books#index'
   devise_for :users
   resources :books, only: :index
-  resources :users, only: :index
+  resources :users, only: [:index, :show]
 end
