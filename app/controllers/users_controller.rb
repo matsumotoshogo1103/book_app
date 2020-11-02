c6a79dd0ea8df7efd82f7697d8654551ef3420c5
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-11-02 16:03:53 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+<<<<<<< Updated upstream
+=======
+  before_action :user_find, only: [:show, :edit, :update]
+>>>>>>> Stashed changes
 
   def index
     @user = User.order('id DESC')
