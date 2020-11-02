b06ead34a231632d7bc4ab99c056aad93d412890
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-11-02 17:13:21 +0900

    コントローラー設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,24 @@ class BooksController < ApplicationController
       redirect_to users_path
     end
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to user_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :image, :price, :autor, :text, :genre_id).merge(user_id: current_user.id)
+  end
+
 end
