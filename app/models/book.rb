b95b02b9b93cea6da78e9957b5536e5e6b616df0
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-11-02 17:13:43 +0900

    モデル（バリデーション）修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,14 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :title
-    validates :genre_id
+    validates :genre_id, numericality: { other_than: 1 }
     validates :autor
-    validates :price
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true }
     validates :text
+    validates :image
     validates :user_id
   end
 end
