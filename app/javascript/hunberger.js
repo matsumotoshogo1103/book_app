5ad7f608870b825d90c0c839d38861f261822693
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-11-05 16:25:04 +0900

    レスポンシブ対応

---
+++ app/javascript/hunberger.js
@@ -0,0 +1,29 @@
+function hunbergerMenu () {
+  const hunberger = document.getElementById("hunberger");
+  const hunbergerList = document.getElementsByClassName("hunberger_list");
+
+  hunberger.addEventListener("click", () => {
+    const hunberger_lists = document.getElementById("hunberger_lists")
+    if (hunberger_lists.getAttribute("style") =="display:block;") {
+      hunberger_lists.removeAttribute("style", "display:block;");
+    } else {
+      hunberger_lists.setAttribute("style", "display:block;");
+    }
+  })
+
+
+  // hunbergerList.addEventListener("mouseover", () => {
+  //   console.log("こんにちは");
+  //   // this.setAttribute("style", "background-color: red;")
+  // })
+
+  // hunbergerList.addEventListener("mouceout", () => {
+  //   this.removeAttribute("style", "background-color: red;")
+  // })
+
+  
+
+}
+
+setInterval(hunbergerMenu, 1000);
+
