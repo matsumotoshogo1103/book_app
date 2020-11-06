1fdb9949371e212a7d62c8f4631764aa843a5d8e
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-11-06 16:50:02 +0900

    header animation

--- app/javascript/hunberger.js
+++ app/javascript/hunberger.js
@@ -1,7 +1,6 @@
 if (document.URL.match( /users/ )) {
-  window.addEventListener("load",() => {
+  function hunberger () {
     const hunberger = document.getElementById("hunberger");
-    const hunbergerList = document.getElementsByClassName("hunberger_list");
 
     hunberger.addEventListener("click", () => {
       const hunberger_lists = document.getElementById("hunberger_lists")
@@ -11,14 +10,9 @@ if (document.URL.match( /users/ )) {
         hunberger_lists.setAttribute("style", "display:block;");
       }
     })
-  }) 
-}
-    // hunbergerList.addEventListener("mouseover", () => {
-    //   console.log("こんにちは");
-    //   // this.setAttribute("style", "background-color: red;")
-    // })
+  }
 
-    // hunbergerList.addEventListener("mouceout", () => {
-    //   this.removeAttribute("style", "background-color: red;")
-    // })
+  setInterval(hunberger, 1000);
+}
+
 
