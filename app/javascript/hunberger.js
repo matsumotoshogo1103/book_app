c414733a72edc5bcff32ea8a99771183f8a5b9ad
matsumotoshogo1103 <vent.se.vuelve@gmail.com>
2020-11-06 16:25:06 +0900

    ホバー時、クリック時のアニメーション設定

--- app/javascript/hunberger.js
+++ app/javascript/hunberger.js
@@ -1,29 +1,24 @@
-function hunbergerMenu () {
-  const hunberger = document.getElementById("hunberger");
-  const hunbergerList = document.getElementsByClassName("hunberger_list");
+if (document.URL.match( /users/ )) {
+  window.addEventListener("load",() => {
+    const hunberger = document.getElementById("hunberger");
+    const hunbergerList = document.getElementsByClassName("hunberger_list");
 
-  hunberger.addEventListener("click", () => {
-    const hunberger_lists = document.getElementById("hunberger_lists")
-    if (hunberger_lists.getAttribute("style") =="display:block;") {
-      hunberger_lists.removeAttribute("style", "display:block;");
-    } else {
-      hunberger_lists.setAttribute("style", "display:block;");
-    }
-  })
-
-
-  // hunbergerList.addEventListener("mouseover", () => {
-  //   console.log("こんにちは");
-  //   // this.setAttribute("style", "background-color: red;")
-  // })
-
-  // hunbergerList.addEventListener("mouceout", () => {
-  //   this.removeAttribute("style", "background-color: red;")
-  // })
-
-  
-
+    hunberger.addEventListener("click", () => {
+      const hunberger_lists = document.getElementById("hunberger_lists")
+      if (hunberger_lists.getAttribute("style") =="display:block;") {
+        hunberger_lists.removeAttribute("style", "display:block;");
+      } else {
+        hunberger_lists.setAttribute("style", "display:block;");
+      }
+    })
+  }) 
 }
+    // hunbergerList.addEventListener("mouseover", () => {
+    //   console.log("こんにちは");
+    //   // this.setAttribute("style", "background-color: red;")
+    // })
 
-setInterval(hunbergerMenu, 1000);
+    // hunbergerList.addEventListener("mouceout", () => {
+    //   this.removeAttribute("style", "background-color: red;")
+    // })
 
